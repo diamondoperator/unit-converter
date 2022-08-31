ddc83e8a97b7ce92af218b050f8d8a5511d1bc20
hackermann20xx <hackerman20xx@gmail.com>
2022-08-31 17:44:29 -0400

    add function

---
+++ index.js
@@ -0,0 +1,17 @@
+//asign elements to variables
+
+
+//get value from input field
+
+
+//function to convert 
+function convert(value){
+
+    //set results to variables
+
+    //do the conversion
+
+    //render to DOM
+
+
+}
