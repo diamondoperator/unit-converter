3a0f677d4ffbaad0d3731e1be2468aeac7849209
hackermann20xx <hackerman20xx@gmail.com>
2022-09-01 01:24:47 -0400

    add DOM manipulation

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ convertButton.addEventListener("click",function(){
     console.log("clicked");
     console.log(input);
 
+
+    let length = document.getElementById("length");
+
     //meters to feet
     let feet = input * 3.281
     console.log(`${input} meters = ${feet.toFixed(3)} feet`);
@@ -20,6 +23,12 @@ convertButton.addEventListener("click",function(){
     let meters = input / 3.281
     console.log(`${input} feet = ${meters.toFixed(3)} meters`);
 
+    length.textContent = `${input} meters = ${feet.toFixed(3)} feet  | 
+     ${input} feet = ${meters.toFixed(3)} meters
+    `
+
+
+    let volume = document.getElementById("volume");
 
     //liters to gallons
     let gallons = input * 0.264;
@@ -28,11 +37,22 @@ convertButton.addEventListener("click",function(){
     let liters = input / 0.264
     console.log(`${input} gallons = ${liters.toFixed(3)} liters`);
 
+    volume.textContent = `
+    ${input} liters = ${gallons.toFixed(3)} gallons  |  
+    ${input} gallons = ${liters.toFixed(3)} liters
+    `
+
+    let mass = document.getElementById("mass");
     //kilograms to pounds
     let pounds = input * 2.204;
     console.log(`${input} kilograms = ${pounds.toFixed(3)} pounds`);
     //pounds to kilograms
     let kilograms = input / 2.204;
     console.log(`${input} pounds = ${kilograms.toFixed(3)} kilograms`);
+
+    mass.textContent = `
+    ${input} kilograms = ${pounds.toFixed(3)} pounds  |  
+    ${input} pounds = ${kilograms.toFixed(3)} kilograms
+    `
 
 })
