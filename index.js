e947f171d359aef48b967f7021e7ab353e4ec0b4
hackermann20xx <hackerman20xx@gmail.com>
2022-09-02 01:07:11 -0400

    add comments

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
-
 //get value from input field
 let convertButton = document.getElementById("convert");
 
-/*
+/*coversions
 1 meter = 3.281 feet
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
 
-convertButton.addEventListener("click",function(){
-    let input = document.getElementById("inputValue").value;
-    console.log("clicked");
-    console.log(input);
+convertButton.addEventListener("click", function () {
+  //asign input vale to variable
+  let input = document.getElementById("inputValue").value;
+  console.log("clicked");
+  console.log(input);
 
-
-    let length = document.getElementById("length");
-
-    //meters to feet
-    let feet = input * 3.281
-    console.log(`${input} meters = ${feet.toFixed(3)} feet`);
-    //feet to meters
-    let meters = input / 3.281
-    console.log(`${input} feet = ${meters.toFixed(3)} meters`);
-
-    length.textContent = `${input} meters = ${feet.toFixed(3)} feet  | 
+  //length
+  let length = document.getElementById("length");
+  //meters to feet
+  let feet = input * 3.281;
+  console.log(`${input} meters = ${feet.toFixed(3)} feet`);
+  //feet to meters
+  let meters = input / 3.281;
+  console.log(`${input} feet = ${meters.toFixed(3)} meters`);
+  length.textContent = `${input} meters = ${feet.toFixed(3)} feet  | 
      ${input} feet = ${meters.toFixed(3)} meters
-    `
+    `;
 
-
-    let volume = document.getElementById("volume");
-
-    //liters to gallons
-    let gallons = input * 0.264;
-    console.log(`${input} liters = ${gallons.toFixed(3)} gallons`);
-    //gallons to liters
-    let liters = input / 0.264
-    console.log(`${input} gallons = ${liters.toFixed(3)} liters`);
-
-    volume.textContent = `
+  //volume
+  let volume = document.getElementById("volume");
+  //liters to gallons
+  let gallons = input * 0.264;
+  console.log(`${input} liters = ${gallons.toFixed(3)} gallons`);
+  //gallons to liters
+  let liters = input / 0.264;
+  console.log(`${input} gallons = ${liters.toFixed(3)} liters`);
+  volume.textContent = `
     ${input} liters = ${gallons.toFixed(3)} gallons  |  
     ${input} gallons = ${liters.toFixed(3)} liters
-    `
+    `;
 
-    let mass = document.getElementById("mass");
-    //kilograms to pounds
-    let pounds = input * 2.204;
-    console.log(`${input} kilograms = ${pounds.toFixed(3)} pounds`);
-    //pounds to kilograms
-    let kilograms = input / 2.204;
-    console.log(`${input} pounds = ${kilograms.toFixed(3)} kilograms`);
-
-    mass.textContent = `
+  //mass
+  let mass = document.getElementById("mass");
+  //kilograms to pounds
+  let pounds = input * 2.204;
+  console.log(`${input} kilograms = ${pounds.toFixed(3)} pounds`);
+  //pounds to kilograms
+  let kilograms = input / 2.204;
+  console.log(`${input} pounds = ${kilograms.toFixed(3)} kilograms`);
+  mass.textContent = `
     ${input} kilograms = ${pounds.toFixed(3)} pounds  |  
     ${input} pounds = ${kilograms.toFixed(3)} kilograms
-    `
-
-})
+    `;
+});
